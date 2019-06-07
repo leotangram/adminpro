d3753fa8cb402e425dd08aef1b9a4f86ad337e79
Leonardo Omaña <leotangram@gmail.com>
2019-06-07 11:24:01 -0500

    Output increment component.

--- src/app/components/increment/increment.component.ts
+++ src/app/components/increment/increment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-increment',
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class IncrementComponent implements OnInit {
   @Input() legend: string = 'Leyenda';
   @Input() progress: number = 50;
+
+  @Output() changeValue: EventEmitter<number> = new EventEmitter();
 
   constructor() {}
 
@@ -28,5 +30,6 @@ export class IncrementComponent implements OnInit {
       return;
     }
     this.progress = this.progress + value;
+    this.changeValue.emit(this.progress);
   }
 }
