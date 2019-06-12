670f03778e379b147ac163c3cfac24c767c9acc6
Leonardo Omaña <leotangram@gmail.com>
2019-06-12 10:23:20 -0500

    Add class dynamic JS vanilla, no ngClass.

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -11,9 +11,21 @@ export class AccountSettingsComponent implements OnInit {
 
   ngOnInit() {}
 
-  changeColor(theme: string) {
+  /**
+   * Changes color
+   * @param theme
+   */
+  changeColor(theme: string, link: any) {
     console.log(theme);
     let url = `assets/css/colors/${theme}.css`;
     this.document.getElementById('theme').setAttribute('href', url);
+  }
+
+  applyCheck(link: any) {
+    let selectors: any = document.getElementsByClassName('selector');
+    for (const ref of selectors) {
+      ref.classList.remove('working');
+    }
+    link.classList.add('working');
   }
 }
