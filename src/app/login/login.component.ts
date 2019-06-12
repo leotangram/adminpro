7a31ab6ebe1eab3ba9730050f26184d09b3378d1
Leonardo Omaña <leotangram@gmail.com>
2019-06-12 12:34:28 -0500

    Use script from  index.html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare function init_plugins();
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(public router: Router) {}
 
   ngOnInit() {
+    init_plugins();
   }
 
+  login() {
+    this.router.navigate(['/dashboard']);
+  }
 }
